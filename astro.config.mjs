6f3545b5ee11500f1180c6f7146dcde50e34f8b3
Razvan Timis <razvan.timis@8x8.com>
2022-12-28 11:46:45 +0200

    Start build the resume

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import image from "@astrojs/image";
 
 // https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  })],
+  integrations: [
+    tailwind(),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp'
+    }),
+    mdx()
+  ],
   vite: {
     ssr: {
       external: ["svgo"]
